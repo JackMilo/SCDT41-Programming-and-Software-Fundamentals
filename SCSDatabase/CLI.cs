f6eee1754637a070a0686ee1691442cae6de7b35
JackMilo <91461218+JackMilo@users.noreply.github.com>
2022-05-10 15:51:36 +0100

    Installed additional library, added CLI file.

---
+++ SCSDatabase/CLI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCSDatabase
+{
+    internal class CLI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
