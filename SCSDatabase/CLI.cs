e1f1b8479c12e56bfc64a22458d67e32828c7f0b
Jack Milo <mil21001248@btc.ac.uk>
2022-05-26 10:47:13 +0100

    Added CLI.

--- SCSDatabase/CLI.cs
+++ SCSDatabase/CLI.cs
@@ -30,6 +30,152 @@ namespace SCSDatabase
             }
             return staff[0];
         }
+
+
+
+        static void MaintainanceMenu(IStaff CurrentUser, List<IProperty> Properties, List<IStaff> Staff)
+        {
+            string answer;
+
+            Console.WriteLine("Maintainance Staff Menu");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Book Service [1]");
+            Console.WriteLine("Complete Service [2]");
+            Console.WriteLine("Logout [3]");
+            Console.Write("Input: ");
+            answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    Console.Clear();
+                    BookingMenu(CurrentUser, Properties);
+                    break;
+                case "2":
+                    Console.Clear();
+                    CompleteServiceMenu(CurrentUser, Properties);
+                    break;
+                case "3":
+                    Console.Clear();
+                    CurrentUser = LogIn(Staff);
+                    break;
+            }
+        }
+
+        static void CleaningMenu(IStaff CurrentUser, List<IProperty> Properties, List<IStaff> Staff)
+        {
+            string answer;
+
+            Console.WriteLine("Cleaning Staff Menu");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Book Service [1]");
+            Console.WriteLine("Complete Service [2]");
+            Console.WriteLine("Request Purchase [3]");
+            Console.WriteLine("Logout [4]");
+            Console.Write("Input: ");
+            answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    Console.Clear();
+                    BookingMenu(CurrentUser, Properties);
+                    break;
+                case "2":
+                    Console.Clear();
+                    CompleteServiceMenu(CurrentUser, Properties);
+                    break;
+                case "3":
+                    Console.Clear();
+                    RequestPurchaseMenu(CurrentUser);
+                    break;
+                case "4":
+                    Console.Clear();
+                    CurrentUser = LogIn(Staff);
+                    break;
+            }
+        }
+
+        static void BookingMenu(IStaff CurrentUser, List<IProperty> Properties)
+        {
+            string answer;
+
+            Console.WriteLine("Booking Menu");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Book Significant Service [1]");
+            Console.WriteLine("Book Cosmetic Service [2]");
+            Console.Write("Input: ");
+            answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                int propertyNum = 1;
+                float price = 1;
+                Enums.Levels priority = Enums.Levels.LOW;
+                Enums.SignificantServiceType type = Enums.SignificantServiceType.WATER_DAMAGE;
+
+                Console.WriteLine("Booking Menu");
+                Console.WriteLine("=======================");
+                Console.WriteLine("Property Number: ");
+                propertyNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Price: ");
+                price = (float)Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Priority Level: ");
+                priority = (Enums.Levels)Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Service type");
+                type = (Enums.SignificantServiceType)Convert.ToInt32(Console.ReadLine());
+                CurrentUser.BookSignificantService(Properties[propertyNum], price, priority, type);
+            }
+            else if (answer == "2")
+            {
+                int propertyNum = 1;
+                float price = 1;
+                Enums.Levels priority = Enums.Levels.LOW;
+                Enums.CosmeticServiceType type = Enums.CosmeticServiceType.DUSTING;
+
+                Console.WriteLine("Booking Menu");
+                Console.WriteLine("=======================");
+                Console.WriteLine("Property Number: ");
+                propertyNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Price: ");
+                price = (float)Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Priority Level: ");
+                priority = (Enums.Levels)Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Service type");
+                type = (Enums.CosmeticServiceType)Convert.ToInt32(Console.ReadLine());
+                CurrentUser.BookCosmeticService(Properties[propertyNum], price, type);
+            }
+        }
+
+        static void CompleteServiceMenu(IStaff CurrentUser, List<IProperty> Properties)
+        {
+            int propertyNum = 1;
+            int serviceNum = 1;
+
+            Console.WriteLine("Complete Service Menu");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Property Number: ");
+            propertyNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Service Number: ");
+            serviceNum = Convert.ToInt32(Console.ReadLine());
+            CurrentUser.CompleteService(Properties[propertyNum].Services[serviceNum]);
+        }
+
+        static void RequestPurchaseMenu(IStaff CurrentUser)
+        {
+            float cost;
+            string name;
+            int quantity;
+
+            Console.WriteLine("Cost: ");
+            cost = (float)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Priority Level: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Service type");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            ((Cleaning)CurrentUser).RequestPurchase(cost, name, quantity);
+        }
+
+
+
         static void Main(string[] args)
         {
             // Creating object lists
@@ -38,6 +184,10 @@ namespace SCSDatabase
             List<IStaff> Staff = new List<IStaff>();
             // Marking the current logged in user
             IStaff CurrentUser;
+            // Exit value
+            bool running = true;
+            // Answer holder
+            string answer;
 
             // Adding customers
             Customers.Add(new Customer("Jack", "Danger", "Jack@gmail.com", "07123456789"));
@@ -54,19 +204,25 @@ namespace SCSDatabase
             Properties.Add(new Domestic(Customers[1], "123Street", 300000, Enums.DomesticPropertyType.BUNGALOWS));
             Properties.Add(new Commercial(Customers[2], "123Street", (float)200000.00, Enums.CommercialPropertyType.OFFICE));
 
+            // Adding services
             Properties[0].CreateSignificantService(500, Enums.Levels.MEDIUM, Enums.SignificantServiceType.WATER_DAMAGE);
             Properties[1].CreateCosmeticService(40, Enums.CosmeticServiceType.VACUUMING);
             Properties[2].CreateSignificantService(2000, Enums.Levels.HIGH, Enums.SignificantServiceType.FIRE_DAMAGE);
 
+            // Login process
             CurrentUser = LogIn(Staff);
 
-            if (CurrentUser is Maintainance)
+            while (running)
             {
-                Console.WriteLine("Wombat");
-            }
-            else if (CurrentUser is Cleaning)
-            {
-                Console.WriteLine("Wombat1");
+                Console.Clear();
+                if (CurrentUser is Maintainance)
+                {
+                    MaintainanceMenu(CurrentUser, Properties, Staff);
+                }
+                else if (CurrentUser is Cleaning)
+                {
+                    CleaningMenu(CurrentUser, Properties, Staff);
+                }
             }
         }
     }
--- SCSDatabase/Enums.cs
+++ SCSDatabase/Enums.cs
@@ -26,22 +26,22 @@ namespace SCSDatabase
 
 		public enum Levels
 		{
-			HIGH,
-			MEDIUM,
-			LOW
+			HIGH = 1,
+			MEDIUM = 2,
+			LOW = 3
 		}
 
 		public enum SignificantServiceType
 		{
-			FIRE_DAMAGE,
-			SMOKE_DAMAGE,
-			WATER_DAMAGE
+			FIRE_DAMAGE = 1,
+			SMOKE_DAMAGE = 2,
+			WATER_DAMAGE = 3
 		}
 		public enum CosmeticServiceType
 		{
-			VACUUMING,
-			MOPPING,
-			DUSTING
+			VACUUMING = 1,
+			MOPPING = 2,
+			DUSTING = 3
 		}
 		public enum SigOrCom
         {
